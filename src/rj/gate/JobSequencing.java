e6c5edb745eb6da39ae4fe4bb4a9d0fa97951d7a
Kalyadapu <rkalyada@ms.ds.uhc.com>
2019-07-05 16:13:25 +0530

    small changes to code

--- src/rj/gate/JobSequencing.java
+++ src/rj/gate/JobSequencing.java
@@ -1,12 +1,22 @@
 package rj.gate;
-/*
+/*ALGORITHM:
+ * Sort all the jobs based on the profit in an descreasing order.
+	Let α be the maximum deadline that will define the size of array.
+	Create a solution array S with d slots.
+	Initialize the content of array S with zero.
+	Check for all jobs.
+	If scheduling is possible a lot ith slot of array s to job i.
+	Otherwise look for location (i-1), (i-2)...1.
+	Schedule the job if possible else reject.
+	Return array S as the answer.
+	End.
  * 
  */
 
 public class JobSequencing {
-	static int profits[]={25,100,27,15};
-	static int dead_lines[]={1,0,1,0};
-	static int jobs[]={0,1,2,3};
+	static int profits[]={85,25,16,40,55,19,92,80,15};
+	static int dead_lines[]={4,3,2,2,3,4,1,2,6};
+	static int jobs[]={0,1,2,3,4,5,6,7,8};
 	static int total_profit = 0;
 	static String text = "JOBS: ";
 	static boolean slots[];
@@ -14,20 +24,20 @@ public class JobSequencing {
 		//Sort jobs in decreasing order of profits
 		sortJobs();		
 		slots = new boolean[findMaxDed()];
-
-		for(int i=0,j=0; i < jobs.length ||j < slots.length;i++){
+		System.out.println(slots.length);
+		for(int i=0,j=0; i < jobs.length && j < slots.length;i++){
 			int  index = dead_lines[i];
 			if(isEmpty(index)){
 				slots[index]=true;
 				j++;
-				text=text+"j"+(jobs[i])+" ";
+				text+="j"+(jobs[i])+" ";
 				total_profit+=profits[i];
 			}else{
 				if(isBelowAvailble(index)){
 					index = getBelowIndex(index);
 					slots[index]=true;
 					j++;
-					text+= (jobs[i])+" ";
+					text+= "j"+(jobs[i])+" ";
 					total_profit+=profits[i];
 				}
 			}
