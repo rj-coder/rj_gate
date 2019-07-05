49255613832215c32856ecb1092dfe6ad63b83e9
Kalyadapu <rkalyada@ms.ds.uhc.com>
2019-07-05 13:55:16 +0530

    binarySearch

--- src/rj/gate/BinarySearch.java
+++ src/rj/gate/BinarySearch.java
@@ -1,10 +1,37 @@
 package rj.gate;
 
-public class BinarySearch {
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Random;
+import java.util.SortedSet;
 
-	
+public class BinarySearch {	
+	static int count=0;
 	public static void main(String[] args) {
-		
+		int arr[] = {22,94,15,99,100,34,53};
+		Arrays.sort(arr);		
+		int x=543;
+		if(binarySearch(arr,0,arr.length-1,x)==true)
+			System.out.println("Found: ");
+		else
+			System.out.println("Not found: ");
+
+	}
+
+	static boolean binarySearch(int a[],int low,int high,int x){
+		if(low<=high){
+			int	middle = (low+high)/2;
+			if(x>a[middle]){			
+				count++;
+				return binarySearch(a,middle+1,high, x);}
+			else if(x<a[middle]){
+				count++;
+				return binarySearch(a,low,middle-1,x);}
+			else{count++;
+				return true;}
+		}
+		else 
+			return false;
 	}
 
 }
